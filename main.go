3f0a588ec4dfc94cb10ad6da2982508781938077
Milofow <Milofow@hotmail.nl>
2023-04-10 17:26:05 +0200

    not working

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const moviesDir = "movies"
+	const port = 8080
+
+	// add a handler for the movie files
+	http.Handle("/", addHeaders(http.FileServer(http.Dir(moviesDir))))
+	fmt.Printf("Starting server on %v\n", port)
+	log.Printf("Serving %s on HTTP port: %v\n", moviesDir, port)
+
+	// serve and log errors
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+// addHeaders will act as middleware to give us CORS support
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	}
+}
